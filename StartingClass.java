0370984aac1dd59a7d356c4b7c4988d462327973
Hadagan <MUt7aChe>
2014-11-01 17:17:11 +0000

    Heavily edited StartingClass, created MainActor

--- StartingClass.java
+++ StartingClass.java
@@ -1,5 +1,92 @@
 package mariogame;
 
-public class StartingClass {
+import java.applet.Applet;
+import java.awt.Color;
+import java.awt.Frame;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class StartingClass extends Applet implements Runnable, KeyListener {
+	
+	private MainActor mainActor;
+	
+	@Override
+	public void init() {
+		setSize(800, 480);
+		setBackground(Color.BLACK);
+		setFocusable(true);
+		Frame frame = (Frame) this.getParent().getParent();
+		frame.setTitle("Q-Bot Alpha");
+		addKeyListener(this);
+	}
+
+	@Override
+	public void start() {
+		Thread thread = new Thread(this);
+		thread.start();
+		mainActor = new MainActor();
+	}
+
+	@Override
+	public void stop() {
+	}
+
+	@Override
+	public void destroy() {
+	}
+
+	@Override
+	public void run() {
+		while (true) {
+			repaint();
+			try {
+				Thread.sleep(17);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		switch (e.getKeyCode()) {
+		case KeyEvent.VK_UP:
+			break;
+
+		case KeyEvent.VK_DOWN:
+			break;
+
+		case KeyEvent.VK_LEFT:
+			break;
+
+		case KeyEvent.VK_RIGHT:
+			break;
+
+		case KeyEvent.VK_SPACE:
+			break;
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		switch (e.getKeyCode()) {
+		case KeyEvent.VK_UP:
+			break;
+		case KeyEvent.VK_DOWN:
+			break;
+		case KeyEvent.VK_LEFT:
+			break;
+		case KeyEvent.VK_RIGHT:
+			break;
+		case KeyEvent.VK_SPACE:
+			break;
+
+		}
+	}
 
 }
