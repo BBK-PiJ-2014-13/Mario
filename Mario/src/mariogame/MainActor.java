0f92b493691e48fe4c0e5a84a12beb9cff4d02e0
Hadagan <MUt7aChe>
2014-11-02 17:20:34 +0000

    Added Background class

--- Mario/src/mariogame/MainActor.java
+++ Mario/src/mariogame/MainActor.java
@@ -1,11 +1,21 @@
 package mariogame;
 
 public class MainActor {
-	// In Java, Class Variables should be private so that only its methods can change them.
+	
+	// Constants
+	final int JUMPSPEED = -15;
+	final int MOVESPEED = 5;
+	final int GROUND = 382;
+	
 	public int centerX = 100;
-	public int centerY = 382;
+	public int centerY = GROUND;
 	public boolean jumped = false;
+	public boolean movingLeft = false;
+	public boolean movingRight = false;
 
+	private Background bg1 = StartingClass.bg1;
+	private Background bg2 = StartingClass.bg2;
+	
 	public int speedX = 0;
 	public int speedY = 1;
 
@@ -14,21 +24,24 @@ public class MainActor {
 		// Moves Character or Scrolls Background accordingly.
 		if (speedX < 0) {
 			centerX += speedX;
-		} else if (speedX == 0) {
-			System.out.println("Do not scroll the background.");
-
-		} else {
-			if (centerX <= 150) {
-				centerX += speedX;
-			} else {
-				System.out.println("Scroll Background Here");
-			}
 		}
-
+		if (speedX <= 0) {
+			bg1.speedX = 0;
+			bg2.speedX = 0;
+		}
+		if (speedX <= 200 && speedX > 0) {
+			centerX += speedX;
+		}
+		if (speedX > 0 && centerX > 200) {
+			bg1.speedX = -MOVESPEED;
+			bg1.speedX = -MOVESPEED;
+		}
+		
 		// Updates Y Position
+		centerY += speedY;
 
-		if (centerY + speedY >= 382) {
-			centerY = 382;
+		if (centerY + speedY >= GROUND) {
+			centerY = GROUND;
 		} else {
 			centerY += speedY;
 		}
@@ -37,8 +50,8 @@ public class MainActor {
 		if (jumped == true) {
 			speedY += 1;
 
-			if (centerY + speedY >= 382) {
-				centerY = 382;
+			if (centerY + speedY >= GROUND) {
+				centerY = GROUND;
 				speedY = 0;
 				jumped = false;
 			}
@@ -46,26 +59,44 @@ public class MainActor {
 		}
 
 		// Prevents going beyond X coordinate of 0
-		if (centerX + speedX <= 60) {
-			centerX = 61;
+		if (centerX + speedX <= 20) {
+			centerX = 21;
 		}
 	}
 
 	public void moveRight() {
-		speedX = 6;
+		speedX = MOVESPEED;
 	}
 
 	public void moveLeft() {
-		speedX = -6;
+		speedX = -MOVESPEED;
+	}
+	
+	public void stopRight() {
+		movingRight = false;
+		stop();
+	}
+	
+	public void stopLeft() {
+		movingLeft = false;
+		stop();
 	}
 
 	public void stop() {
-		speedX = 0;
+		if (movingRight == false && movingLeft == false) {
+			speedX = 0;
+		}
+		if (movingRight == false && movingLeft == true) {
+			moveLeft();
+		}
+		if (movingRight == true && movingLeft == false) {
+			moveRight();
+		}
 	}
 
 	public void jump() {
 		if (jumped == false) {
-			speedY = -15;
+			speedY = JUMPSPEED;
 			jumped = true;
 		}
 
--- Mario/src/mariogame/StartingClass.java
+++ Mario/src/mariogame/StartingClass.java
@@ -10,12 +10,13 @@ import java.awt.event.KeyListener;
 import java.net.URL;
 
 public class StartingClass extends Applet implements Runnable, KeyListener {
-	
+
 	private MainActor mainActor;
-	private Image image, character;
+	private Image image, character, background;
 	private URL base;
 	private Graphics second;
-	
+	public static Background bg1, bg2;
+
 	@Override
 	public void init() {
 		setSize(800, 480);
@@ -24,23 +25,25 @@ public class StartingClass extends Applet implements Runnable, KeyListener {
 		Frame frame = (Frame) this.getParent().getParent();
 		frame.setTitle("Q-Bot Alpha");
 		addKeyListener(this);
-		
+
 		try {
 			base = getDocumentBase();
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
 
-
 		// Image Setups
 		character = getImage(base, "data/character.png");
+		background = getImage(base, "data/background.png");
 	}
 
 	@Override
 	public void start() {
+		bg1 = new Background(0, 0);
+		bg2 = new Background(2160, 0);
+		mainActor = new MainActor();
 		Thread thread = new Thread(this);
 		thread.start();
-		mainActor = new MainActor();
 	}
 
 	@Override
@@ -55,6 +58,8 @@ public class StartingClass extends Applet implements Runnable, KeyListener {
 	public void run() {
 		while (true) {
 			mainActor.update();
+			bg1.update();
+			bg2.update();
 			repaint();
 			try {
 				Thread.sleep(17);
@@ -64,7 +69,7 @@ public class StartingClass extends Applet implements Runnable, KeyListener {
 			}
 		}
 	}
-	
+
 	@Override
 	public void update(Graphics g) {
 		if (image == null) {
@@ -72,18 +77,18 @@ public class StartingClass extends Applet implements Runnable, KeyListener {
 			second = image.getGraphics();
 		}
 
-
 		second.setColor(getBackground());
 		second.fillRect(0, 0, getWidth(), getHeight());
 		second.setColor(getForeground());
 		paint(second);
 
-
 		g.drawImage(image, 0, 0, this);
 	}
-	
+
 	@Override
 	public void paint(Graphics g) {
+		g.drawImage(background, bg1.bgX, bg2.bgY, this);
+		g.drawImage(background, bg2.bgX, bg2.bgY, this);
 		g.drawImage(character, mainActor.centerX - 20, mainActor.centerY - 20, this);
 	}
 
@@ -93,11 +98,14 @@ public class StartingClass extends Applet implements Runnable, KeyListener {
 
 	@Override
 	public void keyPressed(KeyEvent e) {
+
 		switch (e.getKeyCode()) {
 		case KeyEvent.VK_UP:
+			System.out.println("Move up");
 			break;
 
 		case KeyEvent.VK_DOWN:
+			System.out.println("Move down");
 			break;
 
 		case KeyEvent.VK_LEFT:
@@ -105,28 +113,42 @@ public class StartingClass extends Applet implements Runnable, KeyListener {
 			break;
 
 		case KeyEvent.VK_RIGHT:
+			mainActor.moveRight();
 			break;
 
 		case KeyEvent.VK_SPACE:
+			mainActor.jump();
 			break;
+
 		}
+
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
 		switch (e.getKeyCode()) {
 		case KeyEvent.VK_UP:
+			System.out.println("Stop moving up");
 			break;
+
 		case KeyEvent.VK_DOWN:
+			System.out.println("Stop moving down");
 			break;
+
 		case KeyEvent.VK_LEFT:
+			mainActor.stop();
 			break;
+
 		case KeyEvent.VK_RIGHT:
+			mainActor.stop();
 			break;
+
 		case KeyEvent.VK_SPACE:
+			System.out.println("Stop jumping");
 			break;
 
 		}
+
 	}
 
 }
